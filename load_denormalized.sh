e3c44d29e3699a009ea9cccf1c0e4573d75f5294
Sam Bogen <samcbogen@gmail.com>
2022-04-05 21:50:18 -0700

    fix error

--- load_denormalized.sh
+++ load_denormalized.sh
@@ -2,4 +2,4 @@
 
 echo $1
 
-unzip -p "$1" | sed 's/\\u0000//g' | psql postgresql://postgres:pass@localhost:11161/ -c "COPY tweets_jsonb (data) FROM STDIN csv quote e'\x01' delimiter e'\x02';"
+unzip -p "$1" | sed 's/\\u0000//g' | psql postgresql://postgres:pass@localhost:11165/ -c "COPY tweets_jsonb (data) FROM STDIN csv quote e'\x01' delimiter e'\x02';"
